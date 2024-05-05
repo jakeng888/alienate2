374c5c65752112da646106c67eb28df8772b6b3f
jakeng888 <ngshinghei@gmail.com>
2024-05-05 16:56:27 +0800

    Initial Commit

---
+++ alienate/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  alienate
+//
+//  Created by ngshinghei on 4/5/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, worlddd!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ alienate/alienateApp.swift
@@ -0,0 +1,17 @@
+//
+//  alienateApp.swift
+//  alienate
+//
+//  Created by ngshinghei on 4/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct alienateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
